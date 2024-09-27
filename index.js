302d8e0edc1901843327f5f07df2f34f5a1fee4b
3xion <88562716+3xionDev@users.noreply.github.com>
2024-09-27 09:23:18 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function compEng() {
+    window.open("/page/compEng.html","_self")
+}
+function compSci() {
+    window.open("/page/compSci.html","_self")
+}
+function cyberSec() {
+    window.open("/page/cyberSec.html","_self")
+}
+function infSys() {
+    window.open("/page/infSys.html","_self")
+}
+function infTech() {
+    window.open("/page/infTech.html","_self")
+}
+function softEng() {
+    window.open("/page/softEng.html","_self")
+}
+function dataSci() {
+    window.open("/page/dataSci.html","_self")
+}
